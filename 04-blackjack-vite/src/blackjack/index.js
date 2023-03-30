1378936f258763714c442a6b2b3a4ad2203ff071
juanpovedapl <juan.poveda@ibm.com>
2023-03-30 11:59:27 -0500

    Se refactorizo el codigo de blackjack creando los modulos descritos a continuacion : 	modificados:     src/blackjack/index.js 	nuevos archivos: src/blackjack/usecases/crear-carta-html.js 	modificados:     src/blackjack/usecases/crear-deck.js 	nuevos archivos: src/blackjack/usecases/export-modules.js 	nuevos archivos: src/blackjack/usecases/pedirCarta.js 	nuevos archivos: src/blackjack/usecases/turno-computadora.js 	nuevos archivos: src/blackjack/usecases/valorCarta.js

--- 04-blackjack-vite/src/blackjack/index.js
+++ 04-blackjack-vite/src/blackjack/index.js
@@ -1,4 +1,4 @@
-import crearNuevoDeck from './usecases/crear-deck.js'
+import {crearDeck, pedirCarta, valorCarta,turnoComputadora, crearCartaHTML } from './usecases/export-modules.js';
 /**
  * 2C = Two of Clubs
  * 2D = Two of Diamonds
@@ -24,89 +24,32 @@ const divCartasComputadora = document.querySelector('#computadora-cartas');
 const puntosHTML = document.querySelectorAll('small');
 
 
-deck=crearNuevoDeck(tipos,especiales);
-
-
-// Esta función me permite tomar una carta
-const pedirCarta = () => {
-
-    if ( deck.length === 0 ) {
-        throw 'No hay cartas en el deck';
-    }
-    const carta = deck.pop();
-    return carta;
-}
-
-// pedirCarta();
-const valorCarta = ( carta ) => {
-
-    const valor = carta.substring(0, carta.length - 1);
-    return ( isNaN( valor ) ) ? 
-            ( valor === 'A' ) ? 11 : 10
-            : valor * 1;
-}
-
-// turno de la computadora
-const turnoComputadora = ( puntosMinimos ) => {
-
-    do {
-        const carta = pedirCarta();
-
-        puntosComputadora = puntosComputadora + valorCarta( carta );
-        puntosHTML[1].innerText = puntosComputadora;
-        
-        // <img class="carta" src="assets/cartas/2C.png">
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `./public/assets/cartas/${ carta }.png`; //3H, JD
-        imgCarta.classList.add('carta');
-        divCartasComputadora.append( imgCarta );
-
-        if( puntosMinimos > 21 ) {
-            break;
-        }
-
-    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
-
-    setTimeout(() => {
-        if( puntosComputadora === puntosMinimos ) {
-            alert('Nadie gana :(');
-        } else if ( puntosMinimos > 21 ) {
-            alert('Computadora gana')
-        } else if( puntosComputadora > 21 ) {
-            alert('Jugador Gana');
-        } else {
-            alert('Computadora Gana')
-        }
-    }, 100 );
-}
-
+deck=crearDeck(tipos,especiales);
 
 
 // Eventos
 btnPedir.addEventListener('click', () => {
 
-    const carta = pedirCarta();
-    
+    const carta = pedirCarta(deck);
+
     puntosJugador = puntosJugador + valorCarta( carta );
     puntosHTML[0].innerText = puntosJugador;
-    
+
     // <img class="carta" src="assets/cartas/2C.png">
-    const imgCarta = document.createElement('img');
-    imgCarta.src = `./assets/cartas/${ carta }.png`; //3H, JD
-    imgCarta.classList.add('carta');
+    const imgCarta = crearCartaHTML(carta);
     divCartasJugador.append( imgCarta );
 
     if ( puntosJugador > 21 ) {
         console.warn('Lo siento mucho, perdiste');
         btnPedir.disabled   = true;
         btnDetener.disabled = true;
-        turnoComputadora( puntosJugador );
+        turnoComputadora( puntosJugador,puntosHTML[1], divCartasComputadora, deck);
 
     } else if ( puntosJugador === 21 ) {
         console.warn('21, genial!');
         btnPedir.disabled   = true;
         btnDetener.disabled = true;
-        turnoComputadora( puntosJugador );
+        turnoComputadora( puntosJugador,puntosHTML[1], divCartasComputadora, deck);
     }
 
 });
@@ -116,18 +59,18 @@ btnDetener.addEventListener('click', () => {
     btnPedir.disabled   = true;
     btnDetener.disabled = true;
 
-    turnoComputadora( puntosJugador );
+    turnoComputadora(puntosJugador,puntosHTML[1], divCartasComputadora, deck);
 });
 
 btnNuevo.addEventListener('click', () => {
 
     console.clear();
     deck = [];
-    deck = crearNuevoDeck(tipos,especiales);
+    deck = crearDeck(tipos,especiales);
 
     puntosJugador     = 0;
     puntosComputadora = 0;
-    
+
     puntosHTML[0].innerText = 0;
     puntosHTML[1].innerText = 0;
 
---
+++ 04-blackjack-vite/src/blackjack/usecases/export-modules.js
@@ -0,0 +1,5 @@
+export { crearDeck }    from './crear-deck.js';
+export { pedirCarta } from './pedirCarta.js';
+export {crearCartaHTML} from './crear-carta-html.js'
+export { turnoComputadora } from './turno-computadora.js'
+export { valorCarta } from './valorCarta.js';
