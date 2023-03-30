5fc4b8738b30bfa5aa45fa15c87a2e98bdbbbe2a
juanpovedapl <juan.poveda@ibm.com>
2023-03-30 15:20:26 -0500

    se cambio la ruta de imgCarta de nuevo y se currio el npm run build

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -8,7 +8,7 @@ export const crearCartaHTML = ( carta ) => {
     if (!carta) throw new Error('La carta es un argumento obligatorio');
 
     const imgCarta = document.createElement('img');
-    imgCarta.src = `/public/assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');
     return imgCarta;
 }
